29922b23dcfcff5228e38a2486228691750e60f6
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-05-06 15:04:29 +0900

    프로젝트 생성

---
+++ src/main/java/com/holix/holix_server/HolixServerApplication.java
@@ -0,0 +1,13 @@
+package com.holix.holix_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolixServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolixServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/holix/holix_server/HolixServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holix.holix_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolixServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
