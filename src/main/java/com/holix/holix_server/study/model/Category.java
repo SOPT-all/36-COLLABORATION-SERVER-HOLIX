4401948312bc46dcca4d6866dc5bd8c41de45478
chounjae <blueskycuj@naver.com>
2025-05-14 18:19:00 +0900

    [Fix]/#7: Category 상수 리팩터링

--- src/main/java/com/holix/holix_server/study/model/Category.java
+++ src/main/java/com/holix/holix_server/study/model/Category.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 public enum Category {
     PASSIONATE("활발한 스터디에 참여해보세요"),
     INSIGHT("북클럽과 세미나로 인사이트 충전"),
-    NEW_STUDY("새로 올라온 강의"),
+    NEW("새로 올라온 강의"),
     RECOMMENDED("추천 멘토링"),
-    FREE_COMMUNITY("무료 커뮤니티");
+    FREE("무료 커뮤니티");
 
     private final String categoryName;
 
