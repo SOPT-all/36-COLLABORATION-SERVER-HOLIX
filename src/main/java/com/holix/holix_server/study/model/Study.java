e987ba142063c73c125316a0612d4b7e3864efce
chounjae <blueskycuj@naver.com>
2025-05-14 17:04:04 +0900

    [Feature]/#7: study, tag 엔티티 및 category ENUM파일로 생성

--- src/main/java/com/holix/holix_server/study/model/Study.java
+++ src/main/java/com/holix/holix_server/study/model/Study.java
@@ -1,4 +1,47 @@
 package com.holix.holix_server.study.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Table(name = "study")
+@Entity
 public class Study {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "study_id", nullable = false)
+    private Long studyId;
+
+    @Column(name = "study_title", nullable = false)
+    private String studyTitle;
+
+    @Column(name = "user", nullable = false)
+    private String user;
+
+    @Column(name = "price", nullable = false)
+    private String price;
+
+    @Column(name = "img_url", nullable = false)
+    private String imgUrl;
+
+    @Column(name = "category", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    protected Study() {
+    }
+
+    protected Study(
+            String title,
+            String user,
+            String price,
+            String imgUrl,
+            Category category
+    ) {
+        this.studyTitle = title;
+        this.user = user;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.category = category;
+    }
 }
---
+++ src/main/java/com/holix/holix_server/study/model/Tag.java
@@ -0,0 +1,31 @@
+package com.holix.holix_server.study.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "tag")
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id", nullable = false)
+    private Long tagId;
+
+    @Column(name = "tag_name", nullable = false)
+    private String tagName;
+
+    @Column(name = "tag_color", nullable = false)
+    private  String tagColor;
+
+    protected Tag(){
+    }
+
+    protected Tag(
+            String tagName,
+            String tagColor
+    ) {
+        this.tagName = tagName;
+        this.tagColor = tagColor;
+    }
+}
