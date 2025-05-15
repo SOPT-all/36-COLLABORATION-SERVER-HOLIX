c224f1e279355a8b01b6d6fd18ebd60bd31360f9
chounjae <blueskycuj@naver.com>
2025-05-15 15:05:32 +0900

    [Feat]/#7: StudyListResponse로 리턴 로직 완성

--- src/main/java/com/holix/holix_server/study/service/StudyService.java
+++ src/main/java/com/holix/holix_server/study/service/StudyService.java
@@ -1,14 +1,16 @@
 package com.holix.holix_server.study.service;
 
 import com.holix.holix_server.study.dto.reponse.StudyListResponseDTO;
+import com.holix.holix_server.study.dto.reponse.StudyResponseDTO;
+import com.holix.holix_server.study.model.Category;
 import com.holix.holix_server.study.model.Study;
 import com.holix.holix_server.study.respository.StudyRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
 public class StudyService {
+
     private final StudyRepository studyRepository;
 
     public StudyService(StudyRepository studyRepository) {
@@ -16,6 +18,18 @@ public class StudyService {
     }
 
     public StudyListResponseDTO findAll() {
-        return studyRepository.findAll();
+        return new StudyListResponseDTO(
+                EntityToDTOList(studyRepository.findByCategory(Category.PASSIONATE)),
+                EntityToDTOList(studyRepository.findByCategory(Category.INSIGHT)),
+                EntityToDTOList(studyRepository.findByCategory(Category.NEW)),
+                EntityToDTOList(studyRepository.findByCategory(Category.RECOMMENDED)),
+                EntityToDTOList(studyRepository.findByCategory(Category.FREE))
+        );
+    }
+
+    private List<StudyResponseDTO> EntityToDTOList(List<Study> studies) {
+        return studies.stream()
+                .map(StudyResponseDTO::new)
+                .toList();
     }
 }
