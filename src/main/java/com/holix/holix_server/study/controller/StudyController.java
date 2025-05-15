84708597f3e529b5df64b34afe8ced963f3c937f
chounjae <blueskycuj@naver.com>
2025-05-15 15:01:22 +0900

    [Feature]/#7: 스터디 controller 완성

--- src/main/java/com/holix/holix_server/study/controller/StudyController.java
+++ src/main/java/com/holix/holix_server/study/controller/StudyController.java
@@ -1,17 +1,16 @@
 package com.holix.holix_server.study.controller;
 
-
 import com.holix.holix_server.global.code.CommonSuccessCode;
 import com.holix.holix_server.global.response.HolixResponse;
-import com.holix.holix_server.sample.dto.response.SampleResponseDTO;
-import com.holix.holix_server.sample.service.SampleService;
 import com.holix.holix_server.study.dto.reponse.StudyListResponseDTO;
+import com.holix.holix_server.study.dto.reponse.StudyResponseDTO;
+import com.holix.holix_server.study.model.Category;
 import com.holix.holix_server.study.service.StudyService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -27,5 +26,4 @@ public class StudyController {
     ) {
         return HolixResponse.success(CommonSuccessCode.OK, studyService.findAll());
     }
-
 }
