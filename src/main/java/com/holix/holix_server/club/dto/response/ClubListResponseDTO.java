baeb1886d1a70ca27025244a2209fae097400cd6
hyeumm <20221159@sungshin.ac.kr>
2025-05-15 20:51:22 +0900

    [Chore]/#8: 내 클럽 조회 api 응답값을 clubs 로 변경합니다

--- src/main/java/com/holix/holix_server/club/dto/response/ClubListResponseDTO.java
+++ src/main/java/com/holix/holix_server/club/dto/response/ClubListResponseDTO.java
@@ -3,7 +3,7 @@ package com.holix.holix_server.club.dto.response;
 import java.util.List;
 
 public record ClubListResponseDTO(
-        List<ClubResponseDTO> clubListResponseDTO
+        List<ClubResponseDTO> clubs
 ) {
     public static ClubListResponseDTO from(List<ClubResponseDTO> clubResponseDTOList) {
         return new ClubListResponseDTO(clubResponseDTOList);
