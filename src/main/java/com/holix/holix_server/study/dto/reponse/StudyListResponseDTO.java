c0e630fd1d614d64ee9b43f3c1c89d73a64d805d
chounjae <blueskycuj@naver.com>
2025-05-15 15:02:35 +0900

    [Fix]/#7: (Study,Tag)ResponseDTO json에 맞게 수정

--- src/main/java/com/holix/holix_server/study/dto/reponse/StudyListResponseDTO.java
+++ src/main/java/com/holix/holix_server/study/dto/reponse/StudyListResponseDTO.java
@@ -1,14 +1,29 @@
 package com.holix.holix_server.study.dto.reponse;
 
+import lombok.Getter;
+
 import java.util.List;
 
-public record StudyListResponseDTO(
-        Long id,
-        String title,
-        String leader,
-        String price,
-        String url,
-        List<TagResponseDTO> tags,
-        String category
+@Getter
+public class StudyListResponseDTO {
+        private final List<StudyResponseDTO> passionateStudyResponseDTOList;
+        private final List<StudyResponseDTO> insightStudyResponseDTOList;
+        private final List<StudyResponseDTO> newStudyResponseDTOList;
+        private final List<StudyResponseDTO> recommendedStudyResponseDTOList;
+        private final List<StudyResponseDTO> freeStudyResponseDTOList;
+
+        public StudyListResponseDTO(
+                List<StudyResponseDTO> passionate,
+                List<StudyResponseDTO> insight,
+                List<StudyResponseDTO> news,
+                List<StudyResponseDTO> recommended,
+                List<StudyResponseDTO> free
         ) {
+                this.passionateStudyResponseDTOList = passionate;
+                this.insightStudyResponseDTOList = insight;
+                this.newStudyResponseDTOList = news;
+                this.recommendedStudyResponseDTOList = recommended;
+                this.freeStudyResponseDTOList = free;
+        }
 }
+
