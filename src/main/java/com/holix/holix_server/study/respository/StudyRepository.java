98e200f9a124a4319bc9f2044bb1b7e5d1ccdc16
chounjae <blueskycuj@naver.com>
2025-05-15 15:06:20 +0900

    [Feat]/#7: findByCategory로 카테고리 분류

--- src/main/java/com/holix/holix_server/study/respository/StudyRepository.java
+++ src/main/java/com/holix/holix_server/study/respository/StudyRepository.java
@@ -1,9 +1,15 @@
 package com.holix.holix_server.study.respository;
 
+import com.holix.holix_server.study.model.Category;
 import com.holix.holix_server.study.model.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Integer> {
+    List<Study> findAllByCategory(Category category);
+
+    List<Study> findByCategory(Category category);
 }
