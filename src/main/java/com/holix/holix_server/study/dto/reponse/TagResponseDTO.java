562bc14560f82970f5507ed2edd6ee986ed6f46c
chounjae <blueskycuj@naver.com>
2025-05-19 19:01:22 +0900

    [fix]/#7: 응답에 맞게 tag_id => tagId, tag_name => tagName 으로 변경

--- src/main/java/com/holix/holix_server/study/dto/reponse/TagResponseDTO.java
+++ src/main/java/com/holix/holix_server/study/dto/reponse/TagResponseDTO.java
@@ -1,8 +1,8 @@
 package com.holix.holix_server.study.dto.reponse;
 
 public record TagResponseDTO(
-        Long id,
-        String tag_name,
-        String tag_color
+        Long tagId,
+        String tagName,
+        String tagColor
 ) {
 }
