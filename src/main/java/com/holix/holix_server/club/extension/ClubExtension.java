d161def051240dfb8907a9d4b39508d6936d2f7e
hyeumm <20221159@sungshin.ac.kr>
2025-05-20 20:10:24 +0900

    [Chore]/#16: formatParticipants 천 원단위로 쉼표를 찍도록 수정합니다

--- src/main/java/com/holix/holix_server/club/extension/ClubExtension.java
+++ src/main/java/com/holix/holix_server/club/extension/ClubExtension.java
@@ -3,6 +3,6 @@ package com.holix.holix_server.club.extension;
 public class ClubExtension {
 
     public static String formatParticipants(Integer participants, Integer maxParticipants) {
-        return String.format("%d명 / %d명", participants, maxParticipants);
+        return String.format("%,d명 / %,d명", participants, maxParticipants);
     }
 }
