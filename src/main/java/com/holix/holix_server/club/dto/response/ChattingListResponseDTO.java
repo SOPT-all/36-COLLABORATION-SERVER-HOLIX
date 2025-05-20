cdb9fe5fd986b6018adbb00ee626d50125fa1656
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2025-05-20 22:26:33 +0900

    [Refactor]/#20: 채팅 DTO 필드명을 변경합니다.

--- src/main/java/com/holix/holix_server/club/dto/response/ChattingListResponseDTO.java
+++ src/main/java/com/holix/holix_server/club/dto/response/ChattingListResponseDTO.java
@@ -2,7 +2,7 @@ package com.holix.holix_server.club.dto.response;
 
 import java.util.List;
 
-public record ChattingListResponseDTO(List<ChattingResponseDTO> chattingResponseDTOList) {
+public record ChattingListResponseDTO(List<ChattingResponseDTO> chattingList) {
     public static ChattingListResponseDTO from(List<ChattingResponseDTO> chattingResponseDTOList){
         return new ChattingListResponseDTO(chattingResponseDTOList);
     }
